cb27bd332e07972c4c4835d5b32d693206da8c00
JeremyGillard <gillard.jeremy@outlook.fr>
2025-07-23 15:51:12 +0200

    feat: change project strategy

--- budgy/lib/budgy/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Budgy.Mailer do
-  use Swoosh.Mailer, otp_app: :budgy
-end
--- budgy/lib/budgy/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Budgy.Repo do
-  use Ecto.Repo,
-    otp_app: :budgy,
-    adapter: Ecto.Adapters.Postgres
-end
